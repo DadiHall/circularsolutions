d1e5dd5dc995fb1a20b82c2fa91eff42c96565ad
DadiHall <dadih32@gmail.com>
2016-10-18 18:28:02 +0000

    mv application.yml from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@github.com:DadiHall/circularsolutions.git'
 
 
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
