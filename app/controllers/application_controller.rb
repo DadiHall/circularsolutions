20c5dfb520a58aa283477eb15afedcc810221b7b
DadiHall <dadih32@gmail.com>
2017-01-04 18:57:53 +0000

    Changed variable name in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
- 	before_action :set_vars
+ 	before_action :set_varx
 
-    def set_vars
+    def set_varx
      @lcas = Lca.all
      @energy_analyses = EnergyAnalysis.all
      @green_accountings = GreenAccounting.all
